41e60ee561b33f5b3ecaf1e19b7773f0492a5441
Nevra Nur Doğan <122152162+NevraNurDogan@users.noreply.github.com>
2023-10-20 19:56:44 +0300

    Update Donguler1.java

--- Donguler1.java
+++ Donguler1.java
@@ -8,21 +8,18 @@ Fibonacci Dizisi: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144   Fib_Hesapla(8)=21*
 		System.out.println("Gormek istediginiz elemanin sirasini yaziniz:");
 		int num=scanner.nextInt();
 		Fib_Hesapla(num);
-		
-		
-
+	
 	}
 	static void Fib_Hesapla(int sira) {
 		int a=1, b=1,c=0;
 		for(int i=2;i<sira;i++) {
-			 c=a+b;
+		 c=a+b;
 	         a=b;
 	         b=c;
 		}
 		System.out.println(sira+". siradaki eleman "+c);
 		
-		
-		
+				
 	}
 
 }
